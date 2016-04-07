31ed5a78a2e02ae0ec05f3ae4b7bd7ef5f9dcb1f
gladnik <nickgladkov@ya.ru>
2016-04-07 21:13:29 +0300

    Added commons-module and tests for user().goTo(place)
    Improved project structure by deletion redundant files

--- steps-module/src/main/java/ru/qatools/school/steps/UserSteps.java
+++ steps-module/src/main/java/ru/qatools/school/steps/UserSteps.java
@@ -3,14 +3,15 @@ package ru.qatools.school.steps;
 import ru.qatools.school.data.Place;
 
 import static org.hamcrest.core.Is.is;
-import static org.hamcrest.core.Is.isA;
 import static org.junit.Assert.assertThat;
+
 
 /**
  * @author lanwen (Merkushev Kirill)
+ * @author gladnik (Gladkov Nikolai)
  */
 public class UserSteps {
-    
+
     private Place place;
 
     private UserSteps() {
@@ -19,14 +20,13 @@ public class UserSteps {
     public static UserSteps user() {
         return new UserSteps();
     }
-    
+
     public UserSteps goTo(Place place) {
         this.place = place;
         return this;
     }
 
-    public UserSteps shouldSeeCurrentPlace(Place place) {
-        assertThat(this.place, is(place));
-        return this;
+    public void shouldBeAtPlace(Place expectedPlace) {
+        assertThat(this.place, is(expectedPlace));
     }
 }
