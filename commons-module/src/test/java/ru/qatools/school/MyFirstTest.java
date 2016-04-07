0e0c69527a117799f8a3bd25c755312cf908d6f0
user <merkushevio@gmail.com>
2016-04-07 20:59:34 +0300

    initial

--- commons-module/src/test/java/ru/qatools/school/MyFirstTest.java
+++ commons-module/src/test/java/ru/qatools/school/MyFirstTest.java
@@ -1,53 +1,21 @@
 package ru.qatools.school;
 
-import com.tngtech.java.junit.dataprovider.DataProvider;
-import com.tngtech.java.junit.dataprovider.DataProviderRunner;
-import com.tngtech.java.junit.dataprovider.UseDataProvider;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import ru.qatools.school.data.Place;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
+import static org.junit.Assert.assertThat;
+import static org.junit.*;
 import static ru.qatools.school.steps.UserSteps.user;
 
-
 /**
- * @author gladnik (Nikolai Gladkov)
+ * @author lanwen (Merkushev Kirill)
  */
-@RunWith(DataProviderRunner.class)
 public class MyFirstTest {
 
-    @DataProvider
-    public static List<Object> places() {
-        List<Object> placesList
-                = new ArrayList<Object>(Arrays.asList(Place.values()));
-        placesList.add(null);
-        return placesList;
-    }
 
     @Test
-    public void shouldBeAtNullWhenDefault() {
-        user().shouldBeAtPlace(null);
+    public void test() {
+        assertThat(user().goTo(Place.HOME), );
     }
-
-    @Test
-    @UseDataProvider("places")
-    public void afterGoSomewhereShouldBeThere(Place place) {
-        user().goTo(place).shouldBeAtPlace(place);
-    }
-
-    @Test
-    @UseDataProvider("places")
-    public void afterGoSomewhereTwiceShouldBeThere(Place place) {
-        user().goTo(place).goTo(place).shouldBeAtPlace(place);
-    }
-
-    @Test
-    public void afterGoSomewhereElseShouldBeThere() {
-        user().goTo(Place.HOME).goTo(Place.AT_YANDEX)
-                .shouldBeAtPlace(Place.AT_YANDEX);
-    }
+    
 }
