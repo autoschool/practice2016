be84fdc0fa40fa89809caa11226c647aeba76bbc
user <merkushevio@gmail.com>
2016-04-07 20:58:59 +0300

    initial

--- steps-module/src/main/java/ru/qatools/school/steps/UserSteps.java
+++ steps-module/src/main/java/ru/qatools/school/steps/UserSteps.java
@@ -2,16 +2,11 @@ package ru.qatools.school.steps;
 
 import ru.qatools.school.data.Place;
 
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
-
-
 /**
  * @author lanwen (Merkushev Kirill)
- * @author gladnik (Gladkov Nikolai)
  */
 public class UserSteps {
-
+    
     private Place place;
 
     private UserSteps() {
@@ -20,14 +15,13 @@ public class UserSteps {
     public static UserSteps user() {
         return new UserSteps();
     }
-
+    
     public UserSteps goTo(Place place) {
         this.place = place;
         return this;
     }
 
-    public UserSteps shouldBeAtPlace(Place expectedPlace) {
-        assertThat("Places should match!", this.place, is(expectedPlace));
-        return this;
+    public UserSteps should() {
+
     }
 }
