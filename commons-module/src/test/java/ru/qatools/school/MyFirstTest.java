1bb709fb509e8aa145c4cde8a640dd8a1f621eb3
vananos <zaiatcivan@gmail.com>
2016-04-07 23:18:43 +0300

    my first auto test

--- commons-module/src/test/java/ru/qatools/school/MyFirstTest.java
+++ commons-module/src/test/java/ru/qatools/school/MyFirstTest.java
@@ -1,14 +1,20 @@
 package ru.qatools.school;
 
 import static ru.qatools.school.steps.UserSteps.user;
+import ru.qatools.school.data.Place;
+import org.junit.Test;
 
 /**
  * @author lanwen (Merkushev Kirill)
  */
 public class MyFirstTest {
     
+	@Test
     public void test() {
-        user().goTo(null); 
+        user().goTo(Place.AT_YANDEX).
+				shouldBeAt(Place.AT_YANDEX).
+				goTo(Place.HOME).
+				shouldBeAt(Place.HOME);
     }
     
 }
