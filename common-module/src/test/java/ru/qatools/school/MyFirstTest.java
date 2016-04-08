a3af08f935bbf190733fb6b23a181e60aa40a568
Yura Kulikov <wearenanoelectric@gmail.com>
2016-04-08 14:33:56 +0300

    minor fix

--- common-module/src/test/java/ru/qatools/school/MyFirstTest.java
+++ common-module/src/test/java/ru/qatools/school/MyFirstTest.java
@@ -1,34 +1,32 @@
 package ru.qatools.school;
 
+import com.tngtech.java.junit.dataprovider.DataProvider;
+import com.tngtech.java.junit.dataprovider.DataProviderRunner;
+import com.tngtech.java.junit.dataprovider.UseDataProvider;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import ru.qatools.school.data.Place;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static ru.qatools.school.steps.UserSteps.user;
 
-/**
- * @author lanwen (Merkushev Kirill)
- *         changed by aasx (Kulikov Yurii)
- */
-
+@RunWith(DataProviderRunner.class)
 public class MyFirstTest {
 
-    @Test
-    public void expectNullWhenGoToNull() {
-        user().goTo(null)
-                .verifyLocation(null);
-    }
-
-   @Test
-    public void expectYandexWhenGoToYandex() {
-        user().goTo(Place.AT_YANDEX)
-                .verifyLocation(Place.AT_YANDEX);
+    @DataProvider
+    public static List<Object> places() {
+        return new ArrayList<Object>(Arrays.asList(Place.values()));
     }
 
     @Test
-    public void expectHomeWhenGoToHome() {
-        user().goTo(Place.HOME)
-                .verifyLocation(Place.HOME);
-    }
+    @UseDataProvider("places")
+    public void forEveryPlaceExpectPlaceWhenGoToPlace(Place place) {
+        user().goTo(place)
+                .verifyLocation(place);
 
+    }
 
 }
