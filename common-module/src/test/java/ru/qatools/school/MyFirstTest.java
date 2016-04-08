a9981ef2e73392c1c01f849d9da7004aa7e807cc
Yura Kulikov <wearenanoelectric@gmail.com>
2016-04-08 13:49:16 +0300

    added Second version of Test using params

--- common-module/src/test/java/ru/qatools/school/MyFirstTest.java
+++ common-module/src/test/java/ru/qatools/school/MyFirstTest.java
@@ -1,31 +1,34 @@
 package ru.qatools.school;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import ru.qatools.school.data.Place;
-
 import static ru.qatools.school.steps.UserSteps.user;
 
 /**
  * @author lanwen (Merkushev Kirill)
- * changed by aasx (Kulikov Yurii)
+ *         changed by aasx (Kulikov Yurii)
  */
+
 public class MyFirstTest {
+
     @Test
-   public void expectNullWhenGoToNull() {
+    public void expectNullWhenGoToNull() {
         user().goTo(null)
                 .verifyLocation(null);
     }
-    @Test
+
+   @Test
     public void expectYandexWhenGoToYandex() {
         user().goTo(Place.AT_YANDEX)
                 .verifyLocation(Place.AT_YANDEX);
     }
+
     @Test
-    public void expectHomeWhenGoToHome(){
+    public void expectHomeWhenGoToHome() {
         user().goTo(Place.HOME)
                 .verifyLocation(Place.HOME);
     }
-
 
 
 }
