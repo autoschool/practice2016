757c127a5982eeb27ecf64bf03e9f9887c16f313
Igor Merkushev <merkushevio@gmail.com>
2016-04-08 23:03:43 +0300

    First test second version change pom, split test, rename methods

--- commons-module/src/test/java/ru/qatools/school/MyFirstTest.java
+++ commons-module/src/test/java/ru/qatools/school/MyFirstTest.java
@@ -3,8 +3,6 @@ package ru.qatools.school;
 import org.junit.Test;
 import ru.qatools.school.data.Place;
 
-import static org.junit.Assert.assertThat;
-import static org.junit.*;
 import static ru.qatools.school.steps.UserSteps.user;
 
 /**
@@ -14,8 +12,17 @@ public class MyFirstTest {
 
 
     @Test
-    public void test() {
-        assertThat(user().goTo(Place.HOME), );
+    public void shouldBeAtYandexAfterGoToYandex() {
+        user().goTo(Place.AT_YANDEX).shouldSeeCurrentPlace(Place.AT_YANDEX);
     }
-    
+
+    @Test
+    public void shouldBeAtHomeAfterGoToHome() {
+        user().goTo(Place.HOME).shouldSeeCurrentPlace(Place.HOME);
+    }
+
+    @Test
+    public void shouldBeNullIfPlaceNull() {
+        user().goTo(null).shouldSeeCurrentPlace(null);
+    }
 }
