e29e4a05b4bbb60e4f9a2306e5b2137ba1fe1a66
Anton <anton-rib@mail.ru>
2016-04-11 02:49:38 +0300

    moved tests to commons-module, removed demo methods, implemented verification methods

--- steps-module/src/main/java/ru/qatools/school/steps/UserSteps.java
+++ steps-module/src/main/java/ru/qatools/school/steps/UserSteps.java
@@ -1,25 +1,14 @@
 package ru.qatools.school.steps;
 
-import org.hamcrest.BaseMatcher;
-import org.hamcrest.Description;
-import org.hamcrest.Matcher;
 import ru.qatools.school.data.Place;
+
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 /**
  * @author lanwen (Merkushev Kirill)
  */
 public class UserSteps {
-    class PlaceMatcher extends BaseMatcher<Place>{
-        public boolean matches(Object o) {
-            return o != null;
-        }
-
-        public void describeTo(Description description) {
-            description.appendText("Place should not be null");
-        }
-    }
-    
     private Place place;
 
     private UserSteps() {
@@ -28,33 +17,14 @@ public class UserSteps {
     public static UserSteps user() {
         return new UserSteps();
     }
-    
+
     public UserSteps goTo(Place place) {
         this.place = place;
         return this;
     }
 
-
-
-    public UserSteps walkTo(String street) {
-        assertThat(street, new BaseMatcher<String>() {
-            public void describeTo(Description description) {
-            }
-            public boolean matches(Object o){
-                if (o == null) return false;
-                if (o.equals("")) return false;
-                return true;
-            }
-        });
-        return this;
-    }
-    public UserSteps ordersCoffee() {
-        return this;
-    }
-    public UserSteps pays() {
-        return this;
-    }
-    public UserSteps shouldSeeCup() {
+    public UserSteps shouldAppearIn(Place place) {
+        assertThat("Places are not equal.", this.place, is(place));
         return this;
     }
 
