1f7f98820a051b3ce4f0d5d889a5dc3c529abc7d
OneginES <kirienko-e@yandex.ru>
2016-04-11 16:23:11 +0300

    Fixing errors and bugs in first autotest

--- commons-module/src/test/java/ru/qatools/school/MyFirstTest.java
+++ commons-module/src/test/java/ru/qatools/school/MyFirstTest.java
@@ -1,12 +1,17 @@
 package ru.qatools.school;
 
+import org.junit.Ignore;
+import org.junit.Test;
+import ru.qatools.school.data.Place;
+
 import static ru.qatools.school.steps.UserSteps.user;
 
 /**
  * @author lanwen (Merkushev Kirill)
+ * @author onegines
  */
 public class MyFirstTest {
-    
+
     @Test
     public void shouldBeAtYandex() {
         user().goTo(Place.AT_YANDEX)
@@ -18,5 +23,21 @@ public class MyFirstTest {
         user().goTo(Place.HOME)
                 .shouldBeAt(Place.HOME);
     }
-    
+
+    @Test
+    public void shouldBeAtNull() {
+        user().goTo(null)
+                .shouldBeAt(null);
+    }
+
+    /*
+        Always FAIL test. Should be @Ignore in real.
+     */
+    @Ignore
+    @Test
+    public void shouldBeAtYandexAfterGoToHome() {
+        user().goTo(Place.HOME)
+                .shouldBeAt(Place.AT_YANDEX);
+    }
+
 }
