1c1873f17cd7560ac5732ea7cd7ab9a348451f10
Ezhvtumane <gogaopel@gmail.com>
2016-04-11 13:29:44 +0300

    Autotest code is written

--- steps-module/src/main/java/ru/qatools/school/steps/UserSteps.java
+++ steps-module/src/main/java/ru/qatools/school/steps/UserSteps.java
@@ -2,11 +2,15 @@ package ru.qatools.school.steps;
 
 import ru.qatools.school.data.Place;
 
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+
 /**
  * @author lanwen (Merkushev Kirill)
  */
 public class UserSteps {
-    
+
     private Place place;
 
     private UserSteps() {
@@ -15,9 +19,14 @@ public class UserSteps {
     public static UserSteps user() {
         return new UserSteps();
     }
-    
+
     public UserSteps goTo(Place place) {
         this.place = place;
+        return this;
+    }
+
+    public UserSteps verifyPlace(Place place) {
+        assertThat("Places doesn't match!", this.place, is(place));
         return this;
     }
 }
