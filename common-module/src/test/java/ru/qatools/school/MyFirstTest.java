f19383391288dbae43292d14e0ac54b6c8ab0683
nastis <nastis.mi@gmail.com>
2016-04-11 23:12:03 +0300

    updated UserSteps class with getter; added 3 tests to MyFirstTestClass class

--- common-module/src/test/java/ru/qatools/school/MyFirstTest.java
+++ common-module/src/test/java/ru/qatools/school/MyFirstTest.java
@@ -1,14 +1,31 @@
-package test.java.ru.qatools.school;
+package ru.qatools.school;
 
-import static main.java.ru.qatools.school.steps.UserSteps.user;
+import org.junit.Assert;
+import org.junit.Test;
+import ru.qatools.school.data.Place;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static ru.qatools.school.steps.UserSteps.user;
 
 /**
  * @author lanwen (Merkushev Kirill)
  */
 public class MyFirstTest {
-    
-    public void test() {
-        user().goTo(null); 
+
+    @Test
+    public void shouldSeeNull() {
+        Assert.assertThat(user().goTo(null).getPlace(), is(equalTo(null)));
+    }
+
+    @Test
+    public void shouldSeeHome() {
+        Assert.assertThat(user().goTo(Place.HOME).getPlace(), is(equalTo(Place.HOME)));
+    }
+
+    @Test
+    public void shouldSeeYandex() {
+        Assert.assertThat(user().goTo(Place.AT_YANDEX).getPlace(), is(equalTo(Place.AT_YANDEX)));
     }
     
 }
