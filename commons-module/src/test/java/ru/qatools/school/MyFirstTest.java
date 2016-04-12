442f510782292065979a2db195adb0144c8e8dd6
chibert <wellofdreams@yandex.ru>
2016-04-12 13:39:16 +0300

    My solution (version 02)
    My solution (version 02):
    - Убрал комментарии в class MyFirstTest.
    - Убрал лишнюю зависимость в commons-module pom.xml.
    - Убрал из проекта junit-4.12.jar.
    - Отформатировал код в class UserSteps.

--- commons-module/src/test/java/ru/qatools/school/MyFirstTest.java
+++ commons-module/src/test/java/ru/qatools/school/MyFirstTest.java
@@ -11,24 +11,18 @@ import static ru.qatools.school.steps.UserSteps.user;
 
 public class MyFirstTest {
 
-    //Отправляем user в место "null" и проверяем, оказался ли он в "null".
-    //Send the user to the place "null" and verify is he in the "null".
     @Test
     public void verifyNullWhenGoToNull() {
         user().goTo(null)
                 .shouldBeAtPlace(null);
     }
 
-    //Отправляем user в место "HOME" и проверяем, оказался ли он в "HOME".
-    //Send the user to the place "HOME" and verify is he in the "HOME".
     @Test
     public void verifyHomeWhenGoToHome() {
         user().goTo(Place.HOME)
                 .shouldBeAtPlace(Place.HOME);
     }
 
-    //Отправляем user в место "AT_YANDEX" и проверяем, оказался ли он в "AT_YANDEX".
-    //Send the user to the place "AT_YANDEX" and verify is he in the "AT_YANDEX".
     @Test
     public void verifyYandexWhenGoToYandex() {
         user().goTo(Place.AT_YANDEX)
--- steps-module/src/main/java/ru/qatools/school/steps/UserSteps.java
+++ steps-module/src/main/java/ru/qatools/school/steps/UserSteps.java
@@ -1,6 +1,7 @@
 package ru.qatools.school.steps;
 
 import ru.qatools.school.data.Place;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
@@ -8,7 +9,7 @@ import static org.junit.Assert.*;
  * @author lanwen (Merkushev Kirill)
  */
 public class UserSteps {
-    
+
     private Place place;
 
     private UserSteps() {
@@ -17,13 +18,13 @@ public class UserSteps {
     public static UserSteps user() {
         return new UserSteps();
     }
-    
+
     public UserSteps goTo(Place place) {
         this.place = place;
         return this;
     }
 
     public void shouldBeAtPlace(Place place) {
-        assertThat("Wrong location!", this.place,is(place));
+        assertThat("Wrong location!", this.place, is(place));
     }
 }
