460a8a5be0a56d9260df77f8df9f1b674daa2af4
kurau <kurau@yandex-team.ru>
2016-04-14 14:06:21 +0300

    add - first web test

---
+++ steps-module/src/main/java/ru/qatools/school/pages/blocks/widgetblocks/WidgetTitle.java
@@ -0,0 +1,14 @@
+package ru.qatools.school.pages.blocks.widgetblocks;
+
+import org.openqa.selenium.Rectangle;
+import ru.yandex.qatools.htmlelements.element.HtmlElement;
+
+/**
+ * Created by kurau.
+ */
+public class WidgetTitle extends HtmlElement {
+
+    public Rectangle getRect() {
+        return null;
+    }
+}
