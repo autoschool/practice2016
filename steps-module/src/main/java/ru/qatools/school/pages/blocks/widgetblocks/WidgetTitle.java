a342d6a1e7bace305fce2b10ee939f10eef15bdb
Gregory <parentalbox@gmail.com>
2016-04-15 00:14:04 +0300

    added scenario#1
    - rewrote all russian names and comms to english
    
    - added to block WidgetTitle a WebElement widgetCity which is found by card-title_primary and returns the city of widget
    - added to block WidgetTitle method getWidgetCity() that returns a widgetCity
    
    - added to DefaultSteps a Step called "Should be city, mentioned in URL" which asserts that city mentioned in widget is that one refecenced from URL
    
    - added neu test with title "City from widget must match city from URL" and a method public void shouldWidgetCityBeExpectedCity() which opens page, then asserts that cities matches

--- steps-module/src/main/java/ru/qatools/school/pages/blocks/widgetblocks/WidgetTitle.java
+++ steps-module/src/main/java/ru/qatools/school/pages/blocks/widgetblocks/WidgetTitle.java
@@ -1,12 +1,21 @@
 package ru.qatools.school.pages.blocks.widgetblocks;
 
 import org.openqa.selenium.Rectangle;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import ru.yandex.qatools.htmlelements.annotations.Name;
 import ru.yandex.qatools.htmlelements.element.HtmlElement;
 
 /**
  * Created by kurau.
  */
 public class WidgetTitle extends HtmlElement {
+
+    @Name("Название города")
+    @FindBy(css = ".card-title__primary")
+    private WebElement widgetCity;
+
+    public WebElement getWidgetCity() {return widgetCity;}
 
     public Rectangle getRect() {
         return null;
