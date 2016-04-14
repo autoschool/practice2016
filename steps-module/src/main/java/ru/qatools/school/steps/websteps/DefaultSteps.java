d9b14ded5cba60ddcb4bdc96bf568f4d53f55288
Igor Merkushev <merkushevio@gmail.com>
2016-04-14 20:58:41 +0300

    init second practice webtest widgets

--- steps-module/src/main/java/ru/qatools/school/steps/websteps/DefaultSteps.java
+++ steps-module/src/main/java/ru/qatools/school/steps/websteps/DefaultSteps.java
@@ -1,11 +1,15 @@
 package ru.qatools.school.steps.websteps;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import ru.qatools.school.pages.MainPage;
+import ru.qatools.school.pages.blocks.WeatherWidget;
 import ru.yandex.qatools.allure.annotations.Step;
 
 import static java.lang.String.format;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.assertThat;
 import static ru.yandex.qatools.htmlelements.matchers.WebElementMatchers.isDisplayed;
 
@@ -30,6 +34,22 @@ public class DefaultSteps {
     @Step("Должны видеть на странице «{0}»")
     public void shouldSee(WebElement element) {
         assertThat("Должны видеть элемент", element, isDisplayed());
+    }
+
+    @Step("Проверяем что заголовок виджета это запрошенный город")
+    public void shouldSeeTitleWidgetEqualCity(String city) {
+        assertThat("Заголовок города равен названию виджета",
+                driver.findElement(By.cssSelector("#text")).getText(), equalTo(city));
+    }
+
+    @Step("Добавляем виджет на страницу")
+    public void addWidgetOnMainPage() {
+        driver.findElement(By.cssSelector("div>.new-card")).click();
+    }
+
+    @Step("Проверяем что на главной странице три виджета")
+    public void shouldSeeAddedWidgets(int size) {
+        assertThat("Должно быть два виджета", size, is(3));
     }
 
     private MainPage onMainPage() {
