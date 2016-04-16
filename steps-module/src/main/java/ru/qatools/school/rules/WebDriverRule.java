08d319b235d55d485f517608bc14603817f7c6e6
arrumm <aar.rumm@gmail.com>
2016-04-16 16:35:28 +0300

    modified searched element class

--- steps-module/src/main/java/ru/qatools/school/rules/WebDriverRule.java
+++ steps-module/src/main/java/ru/qatools/school/rules/WebDriverRule.java
@@ -2,7 +2,11 @@ package ru.qatools.school.rules;
 
 import org.junit.rules.ExternalResource;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
+
+import java.io.File;
 
 /**
  * Created by kurau.
@@ -12,7 +16,13 @@ public class WebDriverRule extends ExternalResource {
     private WebDriver driver;
 
     protected void before() throws Throwable {
-        this.driver = new FirefoxDriver();
+
+        //привет, костыли
+        File pathBinary = new File("D:\\my Programs\\firefox\\firefox.exe");
+        FirefoxBinary Binary = new FirefoxBinary(pathBinary);
+        FirefoxProfile firefoxPro = new FirefoxProfile();
+        this.driver = new FirefoxDriver(Binary,firefoxPro);
+        //this.driver = new FirefoxDriver();
     }
 
     protected void after() {
