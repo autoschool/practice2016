0f336a4c71ab9f2fae5439e8d380633508de3baf
Ivan Gavrilov <gavrilov.ivan@yandex.ru>
2016-04-19 00:16:26 +0300

    Добавлена проверка соответствия ГородВЗаголовке=ГородВURL

--- commons-module/src/test/java/ru/qatools/school/webtests/WeatherWebTest.java
+++ commons-module/src/test/java/ru/qatools/school/webtests/WeatherWebTest.java
@@ -1,6 +1,7 @@
 package ru.qatools.school.webtests;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import ru.qatools.school.pages.MainPage;
@@ -11,6 +12,8 @@ import ru.yandex.qatools.allure.annotations.Title;
 public class WeatherWebTest {
 
     public static final String MOSCOW = "Moscow";
+    public static final String NEMOSCOW = "Omsk";
+
 
     private DefaultSteps defaultSteps;
 
@@ -27,6 +30,21 @@ public class WeatherWebTest {
     public void shouldSeeWidgetOnMainPage() {
         defaultSteps.openMainPageWithCity(MOSCOW);
         defaultSteps.shouldSee(onMainPage().getWeatherWidget().get(0));
+    }
+
+    @Test
+    @Title("Должны видеть в заголовке искомый город")
+    public void shouldBeTrueCity() {
+        defaultSteps.openMainPageWithCity(MOSCOW);
+        defaultSteps.cityShoudBe(NEMOSCOW);
+    }
+
+    @Test
+    @Ignore
+    @Title("Должны получить ошибку по несоответствию города")
+    public void shouldBeFalseCity() {
+        defaultSteps.openMainPageWithCity(MOSCOW);
+        defaultSteps.cityShoudBe(NEMOSCOW);
     }
 
     private MainPage onMainPage() {
