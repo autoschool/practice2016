2aee760288755f57e8652cf90949d61bdf07f716
merkushevio <merkushevio@gmail.com>
2016-04-18 10:26:07 +0300

    edit MainPage

--- steps-module/src/main/java/ru/qatools/school/pages/MainPage.java
+++ steps-module/src/main/java/ru/qatools/school/pages/MainPage.java
@@ -1,6 +1,7 @@
 package ru.qatools.school.pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import ru.qatools.school.pages.blocks.WeatherWidget;
@@ -23,9 +24,40 @@ public class MainPage {
     @FindBy(css = ".card.card_md")
     private List<WeatherWidget> weatherWidget;
 
+    @Name("Список городов")
+    @FindBy(css = ".inplace.inplace_displayed")
+    private List<WebElement> inplace;
+
+    @Name("Добавление нового виджета")
+    @FindBy(css = ".new-card")
+    private WebElement addWidget;
+
+    @Name("Редактируемое поле названия города")
+    @FindBy(css = "input.inplace.inplace_editable")
+    private WebElement editInplace;
+
+    @Name("Список элементов отображения температуры")
+    @FindBy(css = "weather-temperature.md-12")
+    private List<WebElement> listTemperatures;
 
     public List<WeatherWidget> getWeatherWidget() {
         return weatherWidget;
+    }
+
+    public List<WebElement> getInplace() {
+        return inplace;
+    }
+
+    public WebElement getAddWidget() {
+        return addWidget;
+    }
+
+    public WebElement getEditInplace() {
+        return editInplace;
+    }
+
+    public List<WebElement> getListTemperatures() {
+        return listTemperatures;
     }
 
 }
