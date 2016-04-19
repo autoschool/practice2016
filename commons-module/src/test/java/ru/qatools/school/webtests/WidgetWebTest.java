a690c127fe1d30133925747d4e6cfacd2093a81c
Yura Kulikov <wearenanoelectric@gmail.com>
2016-04-19 14:51:57 +0300

    some new features

--- commons-module/src/test/java/ru/qatools/school/webtests/WidgetWebTest.java
+++ commons-module/src/test/java/ru/qatools/school/webtests/WidgetWebTest.java
@@ -4,22 +4,15 @@ import com.tngtech.java.junit.dataprovider.DataProvider;
 import com.tngtech.java.junit.dataprovider.DataProviderRunner;
 import com.tngtech.java.junit.dataprovider.UseDataProvider;
 import org.junit.Before;
-import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExternalResource;
 import org.junit.runner.RunWith;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import ru.qatools.school.data.City;
 import ru.qatools.school.pages.MainPage;
 import ru.qatools.school.rules.WebDriverRule;
 import ru.qatools.school.steps.websteps.DefaultSteps;
 import ru.yandex.qatools.allure.annotations.Title;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.stream.Stream;
 
 @RunWith(DataProviderRunner.class)
@@ -28,23 +21,19 @@ import java.util.stream.Stream;
  * Created by aasx on 14.04.2016.
  */
 public class WidgetWebTest {
-    public static final String MOSCOW = "Moscow";
+    private static DefaultSteps steps;
+    @Rule
+    public WebDriverRule webDriverRule = new WebDriverRule();
 
     @DataProvider
-   /* public static List<String> dataProviderCities() {
-        return new ArrayList<String>(Arrays.asList(City.values()));
-    } */
     public static String[] dataProviderCities() {
         return Stream.of(City.values()).map(City::name).toArray(String[]::new);
     }
-    @Rule
-    public WebDriverRule webDriverRule=new WebDriverRule();
-    private static DefaultSteps steps;
-
 
     @Before
-    public void initSteps() { steps=new DefaultSteps(webDriverRule.getDriver()); }
-
+    public void initSteps() {
+        steps = new DefaultSteps(webDriverRule.getDriver());
+    }
 
     @Test
     @UseDataProvider("dataProviderCities")
@@ -65,13 +54,15 @@ public class WidgetWebTest {
     @Test
     @UseDataProvider("dataProviderCities")
     @Title("Отображается на один виджет больше после нажатия на кнопку добавления виджета")
-    public void shouldSeeNTimesMoreWidgetsWhenAddNWidgets(String cityToUrl){
+    public void shouldSeeNewWidgetWhenAddWidget(String cityToUrl) {
         steps.openMainPageWithCity(cityToUrl);
+        int numberOfWidget = onMainPage().getWeatherWidget().size();
         steps.clickElement(onMainPage().getButtonAddWidget());
-        steps.shouldSee(onMainPage().getWeatherWidget().get(0));
-        steps.shouldSee(onMainPage().getWeatherWidget().get(1));
+        steps.expectNumberOfWeatherWidgets(numberOfWidget+1);
     }
 
-    private MainPage onMainPage() {return new MainPage(webDriverRule.getDriver());}
+    private MainPage onMainPage() {
+        return new MainPage(webDriverRule.getDriver());
+    }
 
 }
--- steps-module/src/main/java/ru/qatools/school/steps/websteps/DefaultSteps.java
+++ steps-module/src/main/java/ru/qatools/school/steps/websteps/DefaultSteps.java
@@ -3,6 +3,7 @@ package ru.qatools.school.steps.websteps;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import ru.qatools.school.pages.MainPage;
+import ru.qatools.school.pages.blocks.WeatherWidget;
 import ru.yandex.qatools.allure.annotations.Step;
 import ru.yandex.qatools.htmlelements.element.HtmlElement;
 
@@ -29,20 +30,30 @@ public class DefaultSteps {
         driver.get(format(MAIN_PAGE, city));
     }
 
-    @Step("Должны видеть на странице «{0}»")
+    @Step("Должны увидеть на странице «{0}»")
     public void shouldSee(HtmlElement element) {
         assertThat("Должны видеть элемент", element, isDisplayed());
     }
 
     @Step("Должны увидеть название города «{0}» на виджете ")
     public void shouldSeeCorrectCityNameAtWidget(String cityFromUrl) {
-        assertThat("Должны видеть на виджете название города, такое как в URL: «{1}»",
+        assertThat("Отображется текст, не соответствующий URL Запросу",
                 onMainPage().getWeatherWidget().get(0).getWidgetTitle()
                         .getCityName().getText(), is(cityFromUrl));
     }
 
+
     @Step("Кликаем на элемент «{0}»")
     public void clickElement(HtmlElement element) {element.click();}
+
+    @Step("Количество виджетов должно быть «{0}» и они должны быть видимы")
+    public void expectNumberOfWeatherWidgets(int expectedQuantity) {
+        assertThat("Количество виджетов неверно",
+                onMainPage().getWeatherWidget().size(), is(expectedQuantity));
+        for (WeatherWidget ww : onMainPage().getWeatherWidget()) {
+            shouldSee(ww);
+        }
+    }
 
     private MainPage onMainPage() {
         return new MainPage(driver);
