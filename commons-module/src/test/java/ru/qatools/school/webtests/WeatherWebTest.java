528b4f986350994f61910ccfc36274cdafb00012
merkushev.io <merkushevio@gmail.com>
2016-04-20 14:40:59 +0400

    add to tp

--- commons-module/src/test/java/ru/qatools/school/webtests/WeatherWebTest.java
+++ commons-module/src/test/java/ru/qatools/school/webtests/WeatherWebTest.java
@@ -18,6 +18,8 @@ public class WeatherWebTest {
 
     @Rule
     public WebDriverRule webDriverRule = new WebDriverRule();
+    @Rule
+    public TPInformerRule tms = new TPInformerRule("merkushevio");
 
 
     @Before
