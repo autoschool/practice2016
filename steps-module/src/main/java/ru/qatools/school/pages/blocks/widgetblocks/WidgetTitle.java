eeff14535aa0506977c1d8bf8b8f4f8e80f5c596
ava1on <sergpetrov13@yandex.ru>
2016-04-23 01:52:02 +0300

    Refactored all WebElement types to HtmlElements

--- steps-module/src/main/java/ru/qatools/school/pages/blocks/widgetblocks/WidgetTitle.java
+++ steps-module/src/main/java/ru/qatools/school/pages/blocks/widgetblocks/WidgetTitle.java
@@ -1,7 +1,6 @@
 package ru.qatools.school.pages.blocks.widgetblocks;
 
 import org.openqa.selenium.Rectangle;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import ru.yandex.qatools.htmlelements.annotations.Name;
 import ru.yandex.qatools.htmlelements.element.HtmlElement;
@@ -13,9 +12,9 @@ public class WidgetTitle extends HtmlElement {
 
     @Name("Город")
     @FindBy(css = ".card-title__primary")
-    private WebElement primaryTitle;
+    private HtmlElement primaryTitle;
 
-    public WebElement getPrimaryTitle(){
+    public HtmlElement getPrimaryTitle(){
         return primaryTitle;
     }
 
