caf43960be2156bfe4e1eadcdc098bd5edc803dc
Mikhail <mihail-maryfich@yandex.ru>
2016-04-26 01:21:49 +0300

    added tests for task2

--- commons-module/src/test/java/ru/qatools/school/webtests/WeatherWebTest.java
+++ commons-module/src/test/java/ru/qatools/school/webtests/WeatherWebTest.java
@@ -10,7 +10,7 @@ import ru.yandex.qatools.allure.annotations.Title;
 
 public class WeatherWebTest {
 
-    public static final String MOSCOW = "Moscow";
+    public static final String CITI = "Moscow";
 
     private DefaultSteps defaultSteps;
 
@@ -23,10 +23,38 @@ public class WeatherWebTest {
     }
 
     @Test
-    @Title("Должны видеть виджет на главной странице")
+    @Title("Должны видеть хотя бы один виджет на главной странице")
     public void shouldSeeWidgetOnMainPage() {
-        defaultSteps.openMainPageWithCity(MOSCOW);
-        defaultSteps.shouldSee(onMainPage().getWeatherWidget().get(0));
+        defaultSteps.openMainPageWithCity(CITI);
+        defaultSteps.shouldSee(onMainPage().getWeatherWidgetList().get(0));
+    }
+
+    @Test
+    @Title("Должны видеть виджет c правильным городом на главной странице")
+    public void shouldSeeCorrectCityName() {
+        defaultSteps.openMainPageWithCity(CITI);
+        defaultSteps.shouldSeeCorrectCityName(CITI);
+    }
+
+    @Test
+    @Title("Виджетов должно стать на один больше")
+    public void shouldSeeOneMoreWidget() {
+        defaultSteps.openMainPageWithCity(CITI);
+        int originCount = onMainPage().getWeatherWidgetList().size();
+        defaultSteps.addWidgit();
+        defaultSteps.widgetCountShouldBe(originCount + 1);
+
+    }
+    @Test
+    @Title("Виджетов должно стать на 100 больше")
+    public void shouldSeeOneHungredMoreWidget() {
+        defaultSteps.openMainPageWithCity(CITI);
+        int originCount = onMainPage().getWeatherWidgetList().size();
+        for (int i = 0; i < 100; i++) {
+            defaultSteps.addWidgit();
+        }
+        defaultSteps.widgetCountShouldBe(originCount + 100);
+
     }
 
     private MainPage onMainPage() {
