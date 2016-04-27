1d983d7eaee3be5ecbfc85ae80455a2c1d307685
totallynotkate <katekocijevska@gmail.com>
2016-04-27 22:26:10 +0300

    Refactor REST-assured tests to check for cities list size

--- api-tests-module/src/test/java/RestassuredTests.java
+++ api-tests-module/src/test/java/RestassuredTests.java
@@ -1,5 +1,33 @@
+import org.apache.http.HttpStatus;
+import org.junit.Test;
+
+import static com.jayway.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+
 /**
  * Created by totallynotkate (Kate Kocijevska).
  */
 public class RestassuredTests {
+    @Test
+    public void shouldGetErrorWhenRequestNegativeCitiesLimit(){
+        given().baseUri("http://weather.lanwen.ru")
+                .basePath("api")
+                .param("limit", -1)
+                .get("cities")
+                .then()
+                .assertThat()
+                .statusCode(400);
+    }
+    @Test
+    public void shouldGetCitiesListWhenRequestMaxIntegerPlusOneCities(){
+        given().baseUri("http://weather.lanwen.ru")
+                .basePath("api")
+                .param("limit", (long)Integer.MAX_VALUE + 1)
+                .get("cities")
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.SC_OK)
+                .and()
+                .body("toSet.size()", is(272));
+    }
 }
