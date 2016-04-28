b183e302783edaa74f6fe808a04b67dbb5ca71c8
totallynotkate <katekocijevska@gmail.com>
2016-04-28 14:23:49 +0300

    Add data classes for Retrofit tests

---
+++ steps-module/src/main/java/ru/qatools/school/apiData/CityJSON.java
@@ -0,0 +1,24 @@
+package ru.qatools.school.apiData;
+
+/**
+ * Created by totallynotkate (Kate Kocijevska).
+ */
+public class CityJSON {
+    public int uid;
+
+    public String name;
+
+    public Country country;
+
+    public int getUid() {
+        return uid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Country getCountry() {
+        return country;
+    }
+}
