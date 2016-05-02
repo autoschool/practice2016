3ae493aea7c9784825bcce0cca846f1d5ecfcfc4
arrumm <aar.rumm@gmail.com>
2016-05-02 11:35:20 +0300

    refactoring RestAssuredTest, add integration with TestPalm

---
+++ api-tests-module/src/main/java/ApiData/ApiDataPreconditions.java
@@ -0,0 +1,20 @@
+package ApiData;
+
+/**
+ * @author arrumm (Roman Arkhipov) on 29.04.2016.
+ */
+public class ApiDataPreconditions {
+
+    public static final String BASE_URL = "http://weather.lanwen.ru";
+
+    public static final String BASE_PATH = "api";
+
+    public static final String SUGGEST_METHOD = "suggest";
+
+    public static final String CITIES_METHOD = "cities";
+
+    public static final String QUERY_PARAM = "query";
+
+    public static final String LIMIT_PARAM = "limit";
+
+}
