13b23a8f3d049301e4e5fb6edcd06765163b4887
ava1on <sergpetrov13@yandex.ru>
2016-05-08 16:57:29 +0300

    Refactored Retfofit test class

--- api-tests-module/src/test/java/ru/qatools/school/apitests/RetrofitAPITests.java
+++ api-tests-module/src/test/java/ru/qatools/school/apitests/RetrofitAPITests.java
@@ -1,10 +1,8 @@
 package ru.qatools.school.apitests;
 
 import org.apache.http.HttpStatus;
-import org.hamcrest.Matchers;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import retrofit2.Call;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -20,7 +18,7 @@ import java.util.List;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.closeTo;
 import static org.hamcrest.Matchers.hasSize;
-import static org.hamcrest.core.Is.is;
+import static org.hamcrest.Matchers.is;
 import static ru.qatools.school.data.Constants.*;
 
 /**
@@ -39,8 +37,7 @@ public class RetrofitAPITests {
     @Title("Должны получить заданное в параметре количество городов в ответе")
     public void shouldReturnRequestedNumberOfCities() throws IOException{
         Cities cities = retrofit.create(Cities.class);
-        Call<List<CitiesResponse>> request= cities.citiesList(LIMIT_VALUE);
-        Response<List<CitiesResponse>> response = request.execute();
+        Response<List<CitiesResponse>> response = cities.citiesList(LIMIT_VALUE).execute();
         assertThat("Код ответа неверный", response.code(), is(HttpStatus.SC_OK));
         assertThat("Неверное количество городов в ответе", response.body(), hasSize(LIMIT_VALUE));
     }
@@ -49,11 +46,10 @@ public class RetrofitAPITests {
     @Title("Значение температуры по шкале Фаренгейта должно соответствовать значению по шкале Цельсия")
     public void shouldReturnCompatibleTemperatureValuesForCelsiusAndFahrenheit() throws IOException{
         Weather weather = retrofit.create(Weather.class);
-        Call<WeatherResponse> request = weather.weather(CITY_PARAMETER, null);
-        Response<WeatherResponse> response = request.execute();
+        Response<WeatherResponse> response = weather.weather(CITY_PARAMETER, null).execute();
         assertThat("Код ответа неверный", response.code(), is(HttpStatus.SC_OK));
         assertThat("Значение температуры не правильно переведено из шкалы Цельсия в шкалу Фаренгейта",
                 response.body().getTemperatures()[2].getValue(),
-                Matchers.is(closeTo(response.body().recalculateCelsiusToFahrenheit(), TEMPERATURE_ERROR)));
+                is(closeTo(response.body().recalculateCelsiusToFahrenheit(), TEMPERATURE_ERROR)));
     }
 }
