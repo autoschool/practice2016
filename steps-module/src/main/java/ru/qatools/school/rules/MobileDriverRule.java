fccbd3480bf472c29e15b82ed457b69a28352728
ava1on <sergpetrov13@yandex.ru>
2016-05-18 21:11:28 +0300

    Adding MobileDriverRule

--- steps-module/src/main/java/ru/qatools/school/rules/MobileDriverRule.java
+++ steps-module/src/main/java/ru/qatools/school/rules/MobileDriverRule.java
@@ -1,7 +1,32 @@
 package ru.qatools.school.rules;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.URL;
+
 /**
  * @author ava1on
  */
 public class MobileDriverRule {
+    private WebDriver driver;
+
+    protected void before() throws Throwable{
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        desiredCapabilities.setCapability("platformName", "Android");
+        desiredCapabilities.setCapability("deviceName", "Android");
+        desiredCapabilities.setCapability("app", "http://autoschool.github.io/files/ya-metro.apk");
+        desiredCapabilities.setCapability("appWaitActivity", "ru.yandex.metro.MainActivity");
+        driver = new RemoteWebDriver(new URL("http://127.0.0.1:4723/wd/hub"), desiredCapabilities);
+    }
+
+    private void after(){
+        driver.close();
+        driver.quit();
+    }
+
+    public WebDriver getDriver() {
+        return driver;
+    }
 }
